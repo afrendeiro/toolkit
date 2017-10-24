946bba9effba0fd36f4a4f8c08dbcccabd2b0602
Andre Rendeiro <afrendeiro@gmail.com>
2017-10-24 14:36:21 +0200

    add HOMER peak calling and peak summarizing function

--- ngs_toolkit/chipseq.py
+++ ngs_toolkit/chipseq.py
@@ -37,12 +37,12 @@ class ChIPSeqAnalysis(ATACSeqAnalysis):
             from_pickle=from_pickle,
             **kwargs)
 
-    def call_peaks_from_comparisons(self, comparison_table, output_dir="{results_dir}/chipseq_peaks", permissive=True):
+    def call_peaks_from_comparisons(self, comparison_table, output_dir="{results_dir}/chipseq_peaks", permissive=True, overwrite=True):
         """
         Call peaks for ChIP-seq samples using an annotation of which samples belong in each comparison and which
         samples represent signal or background.
 
-        :param pd.DataFrame comparison_table: Comparison table with the following required columns:
+        :param pandas.DataFrame comparison_table: Comparison table with the following required columns:
             "comparison_name", "sample_name", "comparison_side", "sample_group".
         :param str output_dir: Parent directory where peaks will be created. Will be created if does not exist.
         :param bool permissive: If incomplete/incoherent comparisons should be skipped or an error should be thrown.
@@ -88,11 +88,160 @@ class ChIPSeqAnalysis(ATACSeqAnalysis):
                 comparison, [s.name for s in signal_samples], [s.name for s in control_samples]
             ))
             # Call peaks
-            call_chipseq_peak_job(
-                signal_samples, control_samples, output_dir=output_dir, name=comparison)
+            if overwrite:
+                macs2_call_chipseq_peak_job(
+                    signal_samples, control_samples, output_dir=output_dir, name=comparison)
+                homer_call_chipseq_peak_job(
+                    signal_samples, control_samples, output_dir=output_dir, name=comparison)
+            else:
+                if not os.path.exists(os.path.join(output_dir, comparison, comparison + "_peaks.narrowPeak")):
+                    macs2_call_chipseq_peak_job(
+                        signal_samples, control_samples, output_dir=output_dir, name=comparison)
+                if not os.path.exists(os.path.join(output_dir, comparison, comparison + "_homer_peaks.narrowPeak")):
+                    homer_call_chipseq_peak_job(
+                        signal_samples, control_samples, output_dir=output_dir, name=comparison)
+                else:
+                    print("Peak files for comparison '{}' already exist. Skipping.".format(comparison))
+
+    def summarize_peaks_from_comparisons(self, comparison_table, output_dir="{results_dir}/chipseq_peaks", permissive=True):
+        """
+        Call peaks for ChIP-seq samples using an annotation of which samples belong in each comparison and which
+        samples represent signal or background.
+
+        :param pandas.DataFrame comparison_table: Comparison table with the following required columns:
+            "comparison_name", "sample_name", "comparison_side", "sample_group".
+        :param str output_dir: Parent directory where peaks will be created. Will be created if does not exist.
+        :param bool permissive: If incomplete/incoherent comparisons should be skipped or an error should be thrown.
+        :raises ValueError: Will be raised if not `permissive` and incomplete/incoherent comparisons are detected.
+        """
+        req_columns = ["comparison_name", "sample_name", "comparison_side", "sample_group"]
+        assert all([col in comparison_table.columns for col in req_columns]), "Comparison table is missing some of the following columns: '{}'.".format(",".join(req_columns))
+
+        # Complement default `output_dir`
+        if "{results_dir}" in output_dir:
+            output_dir = os.path.abspath(output_dir.format(results_dir=self.results_dir))
+
+        # For each comparison, count called peaks
+        peak_counts = pd.Series(name="peak_number")
+        for comparison in comparison_table['comparison_name'].drop_duplicates().sort_values():
+            file = os.path.join(output_dir, comparison, comparison + "_homer_peaks")
+            homer_peaks_to_bed(file + ".narrowPeak", file + ".bed")
+            try:
+                df = pd.read_csv(file + ".bed", sep="\t")
+            except IOError("Peak files for comparison '{}' don't exist.".format(comparison)) as e:
+                if permissive:
+                    print(e)
+                    continue
+                else:
+                    raise e
+            peak_counts[comparison] = df.shape[0]
+        peak_counts.index.name = 'comparison_name'
+        return peak_counts
+
+    def get_consensus_sites(
+            self, comparison_table, peak_dir="{results_dir}/chipseq_peaks", region_type="summits", extension=250,
+            blacklist_bed="wgEncodeDacMapabilityConsensusExcludable.bed"):
+        """
+        Get consensus (union) of enriched sites (peaks) across all comparisons.
+        If `region_type` --> "summits, regions used will be peak summits which will be extended by `extension`
+        before union. Otherwise sample peaks will be used with no modification.
+
+        `blacklist_bed` is a 3 column BED file with genomic positions to exclude from consensus peak set.
+        """
+        import re
+        from tqdm import tqdm
+        import pybedtools
+
+        # Complement default `peak_dir`
+        if "{results_dir}" in peak_dir:
+            peak_dir = os.path.abspath(peak_dir.format(results_dir=self.results_dir))
+
+        for i, comparison in tqdm(enumerate(comparison_table["comparison_name"].drop_duplicates())):
+            peak_file = os.path.join(peak_dir, comparison, comparison + "_peaks.narrowPeak")
+            genome = comparison_table.loc[comparison_table["comparison_name"] == comparison, "comparison_genome"].drop_duplicates().squeeze()
+
+            assert type(genome) is str, "Could not determine genome of comparison '{}'.".format(comparison)
+
+            # Get peaks
+            if region_type == "summits":
+                try:
+                    f = re.sub("_peaks.narrowPeak", "_summits.bed", peak_file)
+                    peaks = pybedtools.BedTool(f).slop(b=extension, genome=genome)
+                except ValueError:
+                    print("Summits for comparison {} ({}) not found!".format(comparison, f))
+                    continue
+            else:
+                try:
+                    peaks = pybedtools.BedTool(peak_file)
+                except ValueError:
+                    print("Peaks for comparison {} ({}) not found!".format(comparison, peak_file))
+                    continue
+            # Merge overlaping peaks within a comparison
+            peaks = peaks.merge()
+            if i == 0:
+                sites = peaks
+            else:
+                # Concatenate all peaks
+                sites = sites.cat(peaks)
+
+        # Merge overlaping peaks across comparisons
+        sites = sites.merge()
+
+        # Filter
+        # remove blacklist regions
+        blacklist = pybedtools.BedTool(os.path.join(self.data_dir, "external", blacklist_bed))
+        # remove chrM peaks and save
+        sites.intersect(v=True, b=blacklist).filter(lambda x: x.chrom != 'chrM').saveas(os.path.join(self.results_dir, self.name + "_peak_set.bed"))
+
+        # Read up again
+        self.sites = pybedtools.BedTool(os.path.join(self.results_dir, self.name + "_peak_set.bed"))
+
+    def calculate_peak_support(self, comparison_table, peak_dir="{results_dir}/chipseq_peaks"):
+        """
+        Calculate a measure of support for each region in peak set
+        (i.e. ratio of samples containing a peak overlapping region in union set of peaks).
+        """
+        import re
+        from tqdm import tqdm
+
+        if "{results_dir}" in peak_dir:
+            peak_dir = os.path.abspath(peak_dir.format(results_dir=self.results_dir))
+
+        # calculate support (number of samples overlaping each merged peak)
+        comps = comparison_table["comparison_name"].drop_duplicates()
+        for i, comparison in tqdm(enumerate(comps)):
+            peak_file = os.path.join(peak_dir, comparison, comparison + "_peaks.narrowPeak")
+
+            if i == 0:
+                support = self.sites.intersect(peak_file, wa=True, c=True)
+            else:
+                support = support.intersect(peak_file, wa=True, c=True)
+
+        try:
+            support = support.to_dataframe()
+        except:
+            support.saveas("_tmp.peaks.bed")
+            support = pd.read_csv("_tmp.peaks.bed", sep="\t", header=None)
+
+        support.columns = ["chrom", "start", "end"] + comps.tolist()
+        support.index = support['chrom'] + ":" + support['start'].astype(str) + "-" + support['end'].astype(str)
+        support.to_csv(os.path.join(self.results_dir, self.name + "_peaks.binary_overlap_support.csv"), index=True)
+
+        # get % of total consensus regions found per sample
+        # m = (
+        #     pd.melt(support, ["chrom", "start", "end"], var_name="sample_name")
+        #     .groupby("sample_name")
+        #     .apply(lambda x: len(x[x["value"] == 1])))
+
+        # divide sum (of unique overlaps) by total to get support value between 0 and 1
+        support["support"] = support[comps].apply(lambda x: sum([i if i <= 1 else 1 for i in x]) / float(len(comps)), axis=1)
+        # save
+        support.to_csv(os.path.join(self.results_dir, self.name + "_peaks.support.csv"), index=True)
+
+        self.support = support
 
 
-def call_chipseq_peak_job(signal_samples, control_samples, output_dir, name):
+def macs2_call_chipseq_peak_job(signal_samples, control_samples, output_dir, name):
     """
     Call ChIP-seq peaks with MACS2 in a slurm job.
 
@@ -111,13 +260,13 @@ def call_chipseq_peak_job(signal_samples, control_samples, output_dir, name):
     if not os.path.exists(output_path):
         os.mkdir(output_path)
 
-    job_name = "macs2_%s" % name
+    job_name = "macs2_{}".format(name)
 
     # Build job script
     # slurm header
     cmd = tk.slurm_header(
         job_name,
-        os.path.join(output_path, name + ".log"),
+        os.path.join(output_path, job_name + ".log"),
         cpus_per_task=4)
 
     # load macs2
@@ -135,3 +284,78 @@ def call_chipseq_peak_job(signal_samples, control_samples, output_dir, name):
 
     # Submit
     tk.slurm_submit_job(job_file)
+
+
+def homer_call_chipseq_peak_job(signal_samples, control_samples, output_dir, name):
+    """
+    Call ChIP-seq peaks with MACS2 in a slurm job.
+
+    :param list signal_samples: Signal Sample objects.
+    :param list control_samples: Background Sample objects.
+    :param list output_dir: Parent directory where MACS2 outputs will be stored.
+    :param str name: Name of the MACS2 comparison being performed.
+    """
+    from pypiper.ngstk import NGSTk
+    import textwrap
+
+    tk = NGSTk()
+
+    output_path = os.path.join(output_dir, name)
+
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    job_name = "homer_findPeaks_{}".format(name)
+
+    # Build job script
+    # slurm header
+    cmd = tk.slurm_header(
+        job_name,
+        os.path.join(output_path, job_name + ".log"),
+        cpus_per_task=4)
+
+    # make tag directory for the signal samples
+    signal_tag_directory = os.path.join(output_dir, "homer_tag_dir_" + name + "_signal")
+    cmd += """
+\t\tmakeTagDirectory {0} {1}
+    """.format(signal_tag_directory, " ".join([s.filtered for s in signal_samples]))
+
+    # make tag directory for the background samples
+    background_tag_directory = os.path.join(output_dir, "homer_tag_dir_" + name + "_background")
+    cmd += """
+\t\tmakeTagDirectory {0} {1}
+    """.format(background_tag_directory, " ".join([s.filtered for s in control_samples]))
+
+    # call peaks
+    output_file = os.path.join(output_dir, name, name + "_homer_peaks.narrowPeak")
+    if not os.path.exists(os.path.join(output_dir, name)):
+        os.makedirs(os.path.join(output_dir, name))
+    cmd += """
+\t\tfindPeaks {signal} -style factor -o {output_file} -i {background}
+""".format(output_file=output_file, background=background_tag_directory, signal=signal_tag_directory)
+
+    # Slurm footer
+    cmd += "\t\t" + tk.slurm_footer() + "\n"
+
+    # Write job to file
+    job_file = os.path.join(output_path, name + ".sh")
+    with open(job_file, "w") as handle:
+        handle.write(textwrap.dedent(cmd))
+
+    # Submit
+    tk.slurm_submit_job(job_file)
+
+
+def homer_peaks_to_bed(homer_peaks, output_bed):
+    """
+    Convert HOMER peak calls to BED format.
+    The fifth column (score) is the -log10(p-value) of the peak.
+
+    :param str homer_peaks: HOMER output with peak calls.
+    :param str output_bed: Output BED file.
+    """
+    df = pd.read_csv(homer_peaks, sep="\t", comment="#", header=None)
+    df['-log_pvalue'] = (-np.log10(df[13])).replace(pd.np.inf, 1000)
+    df['name'] = df[1] + ":" + df[2].astype(str) + "-" + df[3].astype(str)
+
+    df[[1, 2, 3, 'name', '-log_pvalue']].sort_values("-log_pvalue", ascending=False).to_csv(output_bed, header=False, index=False, sep="\t")

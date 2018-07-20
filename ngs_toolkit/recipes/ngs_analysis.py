d579d5cad933c4d1a1a1983dbc7364a074ecf892
Andre Rendeiro <afrendeiro@gmail.com>
2018-07-20 11:19:57 +0200

    add unbuferring to ngs_analysis recipe

--- ngs_toolkit/recipes/ngs_analysis.py
+++ ngs_toolkit/recipes/ngs_analysis.py
@@ -36,6 +36,21 @@ matplotlib.rcParams["svg.fonttype"] = "none"
 matplotlib.rc('text', usetex=False)
 
 
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+sys.stdout = Unbuffered(sys.stdout)
+
+
 def add_args(parser):
     """
     Global options for analysis.

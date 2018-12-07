4cf7b563f1954bef15a24f1ac23547a4816c164e
Andre Rendeiro <afrendeiro@gmail.com>
2018-12-07 14:48:56 +0100

    add optional requirements for single cell analysis; more explicit requirement separation

--- setup.py
+++ setup.py
@@ -22,9 +22,17 @@ except ImportError:
 with open(os.path.join("ngs_toolkit", "_version.py"), 'r') as handle:
     version = handle.readline().split()[-1].strip("\"'\n")
 
-requirements = open("requirements.txt").read().strip().split("\n")
+requirements = open("requirements/requirements.txt").read().strip().split("\n")
 requirements = [r for r in requirements if not r.startswith("#")]
 requirements = [r for r in requirements if "#egg=" not in r]
+
+requirements_rpy2 = open("requirements/requirements.rpy2.txt").read().strip().split("\n")
+requirements_rpy2 = [r for r in requirements_rpy2 if not r.startswith("#")]
+requirements_rpy2 = [r for r in requirements_rpy2 if "#egg=" not in r]
+
+requirements_sc = open("requirements/requirements.single_cell.txt").read().strip().split("\n")
+requirements_sc = [r for r in requirements_sc if not r.startswith("#")]
+requirements_sc = [r for r in requirements_sc if "#egg=" not in r]
 
 
 # Recipes
@@ -32,7 +40,10 @@ recipes = glob.glob(os.path.join(os.path.curdir, "ngs_toolkit", "recipes", "*.py
 recipes = list(map(
     lambda x: x.replace(".py", "").replace("./", "").replace("/", "."),
     recipes))
-recipes = [" = ".join([i, j + ":main"]) for i, j in zip(map(lambda x: x.split('.')[-1], recipes), recipes)]
+recipes = [
+    " = ".join([i, j + ":main"])
+    for i, j in
+    zip(map(lambda x: x.split('.')[-1], recipes), recipes)]
 recipes = [r for r in recipes if not r.startswith("__init__")]
 
 
@@ -63,17 +74,21 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
     ],
-    keywords="bioinformatics, sequencing, ngs, ngs analysis, ATAC-Seq, ChIP-seq, RNA-seq, project management",
+    keywords="bioinformatics, sequencing, ngs, ngs analysis, " +
+             "ATAC-Seq, ChIP-seq, RNA-seq, project management",
     url="https://github.com/afrendeiro/toolkit",
     author=u"Andre Rendeiro",
     author_email='afrendeiro@gmail.com',
     license="GPL3",
     install_requires=requirements,
     extras_require={
-        'deseq2':  ["rpy2==2.8.6"]},
+        'deseq2':  requirements_rpy2,
+        'single_cell': requirements_sc},
     package_data={
         'ngs_toolkit': ['config/*.yaml']
     },
-    data_files=["requirements.txt"],
+    data_files=[
+        "requirements/requirements.txt",
+        "requirements/requirements.single_cell.txt"],
     **extra
 )
